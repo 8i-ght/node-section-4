e43c908a71f1b2f88ef55a4c95db58910a3e5af4
8i-ght <needboosts3545@gmail.com>
2024-03-22 14:32:14 -0500

    on video 78

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+let {people} = require('./data');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    if(url === '/') {
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.write('<h1>Home Page</h1>');
-        res.end();
-    } else if(url === '/about') {
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.write('<h1>About Page</h1>');
-        res.end();
-    } else {
-        res.writeHead(404, {'content-type': 'text/html'});
-        res.write('<h1>Page Not Found</h1>');
-        res.end();
-    }
-   
-});
+// static assets
+app.use(express.static('./methods-public'));
 
-server.listen(5000);  
 
-// NodeJS Fix : 35:55
+app.get('/api/people', (req, res) => {
+  res.status(200).json({success: true, data: people});
+  console.log('on people page')
+})
+ 
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000...');
+})
+
+// Video 78
