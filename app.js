179b3a5b161a2825a81f8ee8558207b6a63c8b4c
8i-ght <needboosts3545@gmail.com>
2024-04-01 12:09:59 -0500

    finish all

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
-let {people} = require('./data');
+
+const people = require('./routes/people');
+const auth = require('./routes/auth');
 
 // static assets
 app.use(express.static('./methods-public'));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
 
+// parse json
+app.use(express.json());
 
-app.get('/api/people', (req, res) => {
-  res.status(200).json({success: true, data: people});
-  console.log('on people page')
-})
- 
+app.use('/api/people', people);
+app.use('/login', auth);
 
 app.listen(5000, () => {
   console.log('server is listening on port 5000...');
 })
 
-// Video 78
+// Video 85
